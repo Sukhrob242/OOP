c141708e80d6a27ac6d16f06b5286022428f7353
Sukhrobjon <79089497+Sukhrob242@users.noreply.github.com>
2022-08-23 12:19:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,4 @@
-// class Component {
-//     constructor(selector){
-//         this.$el = document.querySelector(selector)
-//     }
-//     showElement() {
-//          this.$el.style.display = 'block' 
-//     }
 
-//     hideElement() {
-//         this.$el.style.display = 'none' 
-//     }
-// }
-
-// class Circle extends Component {
-//     constructor (options){
-//         super(options.selector);
-//         this.$el.style.width = this.$el.style.height = options.size + 'px' ;
-//         this.$el.style.background = options.color
-//         this.$el.style.borderRadius = "50%"
-//     }
-// }
-
-// const circle1 = new Circle({
-//     selector:"#circle1",
-//     size: 100,
-//     color: 'crimson' 
-// })
-
-// const circle2 = new Circle({
-//     selector:"#circle2",
-//     size: 100,
-//     color: 'coral' 
-// })
 
 let radius = 1 ;
 let x = 1 ; 
